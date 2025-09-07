cc6743388adec9ee2adc510ae62a626ea9138c98
David Bradway <david.bradway@gmail.com>
2025-09-07 17:08:00 -0400

    first commit

---
+++ go.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from tinydb import TinyDB, Query
+from flask import Flask, render_template
+
+app = Flask(__name__)
+db = TinyDB("db.json")
+
+
+@app.route("/")
+def root():
+    all_items = db.all()
+    return render_template("table.html", items=all_items)
+
+
+@app.route("/i/<int:item_id>")
+def create(item_id):
+    data = {"item_id": "i" + str(item_id), "datetime": datetime.now().isoformat()}
+    db.insert(data)
+
+    items = Query()
+    all_items = db.search(items.item_id == "i" + str(item_id))
+
+    week_count = 0
+    unique_weeks = []
+    for item in all_items:
+        # convert back to datetime
+        dt = datetime.fromisoformat(item["datetime"])
+        week_number = dt.isocalendar().week
+        # find the number of unique weeks
+        if week_number not in unique_weeks:
+            unique_weeks.append(week_number)
+            week_count = week_count + 1
+    return f"<h1>{week_count} week(s)</h1>"
+
+
+@app.route("/json")
+def json():
+    all_items = db.all()
+    return str(all_items)
---
+++ readme.md
@@ -0,0 +1,11 @@
+'''
+cd biketraintracker
+python -m venv venv
+.\venv\Scripts\Activate
+pip install -r requirements.txt
+
+flask --app go run --host=0.0.0.0
+go to http://127.0.0.1:5000/ in the browswer
+go to http://127.0.0.1:5000/i/1 in the browswer
+go to http://127.0.0.1:5000/json in the browswer
+'''
